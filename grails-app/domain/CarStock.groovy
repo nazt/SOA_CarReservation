ec0bcb6da232345ecc5abfa82623fd07bbf6f0b1
Nat <nat.weerawan@gmail.com>
2009-10-08 16:42:08 +0700

    add CarStock hasMany reservedDetail

--- grails-app/domain/CarStock.groovy
+++ grails-app/domain/CarStock.groovy
@@ -1,5 +1,6 @@
 class CarStock {
 	static belongsTo = [colour:Colour,brand:CarBrand,engine:Engine,fuel:Fuel,type:CarType,gear:Gear]
+	static hasMany = [revervedDetail:ReservedDetail]
 	String model
 	String carRegistration
 	Boolean reserved
