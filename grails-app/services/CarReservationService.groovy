1c6b6b4a276bb6381c511b1f0053135c316ee3dc
Nat <nat.weerawan@gmail.com>
2009-10-16 14:55:04 +0700

    add getColours() in service

--- grails-app/services/CarReservationService.groovy
+++ grails-app/services/CarReservationService.groovy
@@ -1,8 +1,13 @@
 class CarReservationService {
 
     boolean transactional = true
+ 	static expose=['xfire']
 
     def serviceMethod() {
 
     }
+	Colour[] getColours()
+	{
+		Colour.list() as Colour[]
+	}
 }
